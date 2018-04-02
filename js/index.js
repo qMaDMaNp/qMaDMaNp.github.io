f5002c9232bba64ef3759f577cb3345ed1246d1d
unknown <medifman@gmail.com>
2018-04-02 16:57:43 +0300

    Nava v3.2 BEM

--- js/index.js
+++ js/index.js
@@ -1,7 +1,9 @@
 $('.hamburger').click(function() {
 		$('.navigation-list').toggleClass('show-navigation-list');
+		$('.hamburger').toggleClass("change");
 });
 
 window.onscroll = function() {
   $('.navigation-list').removeClass('show-navigation-list');
+	$('.hamburger').removeClass("change");
 };
